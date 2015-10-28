03f313df85dfeffaf7eecd50a7c117cb422e443c
Timur <timurhan.s@gmail.com>
2015-10-28 22:00:28 +0100

    Remove unneeded dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,7 @@
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.taoensso/timbre "3.3.1"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [camel-snake-kebab "0.3.2"]
-                 [metam/core "1.0.7"]]
+                 [camel-snake-kebab "0.3.2"]]
   :source-paths ["src/main/clj"]
   :plugins [[lein-modules "0.3.11"]]
   :resource-paths ["src/main/resources"])
