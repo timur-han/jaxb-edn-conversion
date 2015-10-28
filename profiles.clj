4c289cc4f8b8f4e8649f0aa86ad5ba1db9640d05
Timur <timurhan.s@gmail.com>
2015-10-28 21:18:59 +0100

    Add codox as documentation generator

--- profiles.clj
+++ profiles.clj
@@ -2,4 +2,5 @@
                       [org.clojure/tools.nrepl "0.2.7"]
                       [midje "1.7.0"]]
        :plugins [[lein-kibit "0.1.2"]
+                 [lein-codox "0.9.0"]
                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
