17752bcfc30c60dad849eb387132f231a6fa9ba5
Timur <timurhan.s@gmail.com>
2015-10-28 21:55:04 +0100

    Update project information in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject uni-stuttgart.ipsm/jaxb-edn-conversion "0.0.1-SNAPSHOT"
+  :description "A library to convert jaxb objects into edn maps and vice versa"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :comments "same as Clojure"}
+  :min-lein-version "2.5.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.taoensso/timbre "3.3.1"]
                  [org.clojure/data.xml "0.0.8"]
